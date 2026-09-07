b9e7d67a46658c3ae95a077235b2be6ad6245004
astropuzzo <gianmarcospagnoli345@gmail.com>
2026-09-07 18:57:10 +0200

    chore: align minimum application version with NINA 3.3 nightly #057

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: Guid("bf861692-b3de-4fdc-8a74-d2b97434f49d")]
-[assembly: AssemblyMetadata("MinimumApplicationVersion", "3.2.0.9001")]
+[assembly: AssemblyMetadata("MinimumApplicationVersion", "3.3.0.1057")]
 [assembly: AssemblyMetadata("License", "Apache-2.0")]
 [assembly: AssemblyMetadata("LicenseURL", "https://www.apache.org/licenses/LICENSE-2.0")]
 [assembly: AssemblyMetadata("Repository", "https://github.com/astropuzzo/QualitySessionMeter")]
