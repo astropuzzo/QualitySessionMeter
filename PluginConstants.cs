17249e1f43052029ced2aaa6382ae505bcd19a2b
astropuzzo <gianmarcospagnoli345@gmail.com>
2026-09-07 17:30:52 +0200

    feat(v1): add plugin identity

---
+++ PluginConstants.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace NINA.Plugin.QualitySessionMeter;
+
+internal static class PluginConstants {
+    public static readonly Guid Identifier = Guid.Parse("bf861692-b3de-4fdc-8a74-d2b97434f49d");
+    public const string Name = "QualitySessionMeter";
+    public const string DisplayName = "Quality Session Meter";
+}
