a04e161295ece4d105c6e79f443c8467435faee5
astropuzzo <gianmarcospagnoli345@gmail.com>
2026-09-07 17:30:59 +0200

    feat(v1): add plugin manifest metadata

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: Guid("bf861692-b3de-4fdc-8a74-d2b97434f49d")]
+[assembly: AssemblyMetadata("MinimumApplicationVersion", "3.2.0.9001")]
+[assembly: AssemblyMetadata("License", "MIT")]
+[assembly: AssemblyMetadata("LicenseURL", "https://opensource.org/license/mit")]
+[assembly: AssemblyMetadata("Repository", "https://github.com/astropuzzo/QualitySessionMeter")]
+[assembly: AssemblyMetadata("Homepage", "https://github.com/astropuzzo/QualitySessionMeter")]
+[assembly: AssemblyMetadata("Tags", "Quality,Subframe,Guiding,Clouds,Session,Imaging")]
+[assembly: AssemblyMetadata("ChangelogURL", "https://github.com/astropuzzo/QualitySessionMeter/blob/main/CHANGELOG.md")]
+[assembly: ComVisible(false)]
