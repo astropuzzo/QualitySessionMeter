9cd10969c878cb56018a624d510b2c7f18b35b1d
astropuzzo <gianmarcospagnoli345@gmail.com>
2026-09-07 17:31:33 +0200

    feat(v1): add quality input model

---
+++ Models/FrameQualityInput.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace NINA.Plugin.QualitySessionMeter.Models;
+
+public sealed class FrameQualityInput {
+    public int FrameIndex { get; init; }
+    public DateTime TimestampUtc { get; init; }
+    public string OriginalPath { get; init; }
+    public string Target { get; init; }
+    public string Filter { get; init; }
+    public double ExposureSeconds { get; init; }
+    public int Gain { get; init; }
+    public int BinX { get; init; }
+    public int BinY { get; init; }
+    public string Camera { get; init; }
+    public int StarCount { get; init; } = -1;
+    public double BackgroundMedian { get; init; } = double.NaN;
+    public BaselineSnapshot Baseline { get; init; }
+    public GuideExposureMetrics Guide { get; init; }
+}
